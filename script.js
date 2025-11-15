0625859dc65bf5a11a912d678893d4da537a9166
Vishwesh Shukla <vishveshshukla82@gmail.com>
2025-11-15 10:46:42 +0530

    Dynamic Table

--- script.js
+++ script.js
@@ -1,38 +1,35 @@
-const iplTeams = [
-    { fullName: "Chennai Super Kings", team: "CSK", captain: "Ruturaj Gaikwad", bgColor: "Yellow", color: "Blue", trophiesCount: 5 },
-    { fullName: "Mumbai Indians", team: "MI", captain: "Hardik Pandya", bgColor: "Blue", color: "Gold", trophiesCount: 5 },
-    { fullName: "Royal Challengers Bengaluru", team: "RCB", captain: "Faf du Plessis", bgColor: "Red", color: "Black", trophiesCount: 1 },
-    { fullName: "Kolkata Knight Riders", team: "KKR", captain: "Shreyas Iyer", bgColor: "Purple", color: "Gold", trophiesCount: 3 },
-    { fullName: "Rajasthan Royals", team: "RR", captain: "Sanju Samson", bgColor: "Pink", color: "Blue", trophiesCount: 1 },
-    { fullName: "Sunrisers Hyderabad", team: "SRH", captain: "Pat Cummins", bgColor: "Orange", color: "Black", trophiesCount: 1 },
-    { fullName: "Delhi Capitals", team: "DC", captain: "Rishabh Pant", bgColor: "Blue", color: "Red", trophiesCount: 0 },
-    { fullName: "Punjab Kings", team: "PBKS", captain: "Shikhar Dhawan", bgColor: "Red", color: "Gold", trophiesCount: 0 },
-    { fullName: "Lucknow Super Giants", team: "LSG", captain: "KL Rahul", bgColor: "Blue", color: "Orange", trophiesCount: 0 },
-    { fullName: "Gujarat Titans", team: "GT", captain: "Shubman Gill", bgColor: "Navy Blue", color: "Gold", trophiesCount: 1 }
-];
-
-
-
-let box = document.querySelector(".box");
+let nameInput = document.querySelector("input[type='text']");
+let ageInput = document.querySelector("input[type='number']");
 let btn = document.querySelector("button");
-let h1 = document.querySelector("h1");
-let fullNameBox = document.querySelector(".full-name");
-let captainName = document.querySelector(".captain");
-let tCount = document.querySelector(".t-count");
-btn.addEventListener("click", function () {
 
-    let randomIndex = Math.floor((Math.random() * iplTeams.length));
-    let obj = iplTeams[randomIndex];
-    renderTeam(obj);
-})
+let table = document.querySelector("table");
+let tableBody = document.querySelector("table tbody");
 
-function renderTeam({ fullName, team, captain, bgColor, color, trophiesCount }) {
-    h1.textContent = team;
-    fullNameBox.textContent = fullName;
-    captainName.textContent = captain;
-    tCount.textContent = trophiesCount;
-    box.style.backgroundColor = bgColor;
-    box.style.color = color;
+function toggleTable() {
+    if (tableBody.childElementCount == 0) {
+        table.style.display = "none";
+    } else {
+        table.style.display = "block";
+    }
 }
 
+toggleTable()
 
+btn.addEventListener("click", function () {
+    if (nameInput.value.length < 3 || (ageInput.value).trim().length < 0) {
+        alert("Please fill valid details : name must be three charcters long :")
+        return;
+    }
+    let tr = document.createElement("tr");
+    let tdName = document.createElement("td");
+    let tdAge = document.createElement("td");
+
+    tdName.textContent = nameInput.value;
+    tdAge.textContent = ageInput.value;
+    nameInput.value = "";
+    ageInput.value = "";
+    tr.appendChild(tdName);
+    tr.appendChild(tdAge);
+    tableBody.appendChild(tr);
+    toggleTable()
+});
