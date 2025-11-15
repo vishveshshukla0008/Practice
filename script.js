03053495ed99c1690d4766e96d1748c5d14f7c6e
Vishwesh Shukla <vishveshshukla82@gmail.com>
2025-11-15 23:09:09 +0530

    basic form logic

--- script.js
+++ script.js
@@ -1,35 +1,87 @@
-let nameInput = document.querySelector("input[type='text']");
-let ageInput = document.querySelector("input[type='number']");
+let form = document.querySelector("form");
+let emailInp = document.querySelector("input[name='email']");
+let passInp = document.querySelector("input[type='password']");
+let phone = document.querySelector("input[type='number']");
 let btn = document.querySelector("button");
 
-let table = document.querySelector("table");
-let tableBody = document.querySelector("table tbody");
+let emailError = document.querySelector(".email-error");
+let passwordError = document.querySelector(".password-error");
+let phoneError = document.querySelector(".phone-error");
 
-function toggleTable() {
-    if (tableBody.childElementCount == 0) {
-        table.style.display = "none";
+//check input values are empty - till disable the button
+
+emailInp.addEventListener("input", checkValidationForValues);
+passInp.addEventListener("input", checkValidationForValues);
+phone.addEventListener("input", checkValidationForValues);
+
+let errorFlag = false;
+
+function checkValidationForValues() {
+    if (
+        emailInp.value.length < 3 ||
+        passInp.value.length < 3 ||
+        phone.value.length < 3
+    ) {
+        btn.disabled = true;
     } else {
-        table.style.display = "block";
+        btn.disabled = false;
     }
 }
 
-toggleTable()
+checkValidationForValues();
 
-btn.addEventListener("click", function () {
-    if (nameInput.value.length < 3 || (ageInput.value).trim().length < 0) {
-        alert("Please fill valid details : name must be three charcters long :")
+form.addEventListener("submit", formSubmit);
+
+function formSubmit(e) {
+    e.preventDefault();
+
+    let formData = new FormData(form);
+    let email = formData.get("email");
+    let password = formData.get("password");
+    let phoneval = formData.get("phone");
+
+    checkError(email, password, phoneval);
+
+    if (errorFlag) {
+        alert("Error occured : please fix the errors");
         return;
     }
-    let tr = document.createElement("tr");
-    let tdName = document.createElement("td");
-    let tdAge = document.createElement("td");
 
-    tdName.textContent = nameInput.value;
-    tdAge.textContent = ageInput.value;
-    nameInput.value = "";
-    ageInput.value = "";
-    tr.appendChild(tdName);
-    tr.appendChild(tdAge);
-    tableBody.appendChild(tr);
-    toggleTable()
-});
+    emailInp.value = "";
+    passInp.value = "";
+    phone.value = "";
+    console.log("login Success");
+    console.log(email);
+    console.log(password);
+    console.log(phoneval);
+}
+
+function checkError(email, password, phone) {
+    const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+
+    if (!emailRegex.test(email)) {
+        emailError.classList.remove("hide");
+        emailError.textContent = "*Invalid email";
+        errorFlag = true;
+        return;
+    }
+    if (password.length < 8) {
+        passwordError.classList.remove("hide");
+
+        passwordError.textContent = "Password must be 8 characters long";
+        errorFlag = true;
+        return;
+    }
+    if (phone.length < 10) {
+        phoneError.classList.remove("hide");
+
+        phoneError.textContent = "Phone number must be length of 10";
+        errorFlag = true;
+        return;
+    }
+
+    errorFlag = false;
+    emailError.classList.add("hide");
+    passwordError.classList.add("hide");
+    phoneError.classList.add("hide");
+}
