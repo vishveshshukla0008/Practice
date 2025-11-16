b1d9189095545bbb7193e1f3252acc5a15dccbdd
Vishwesh Shukla <vishveshshukla82@gmail.com>
2025-11-16 11:20:39 +0530

    UPDATED FORM

--- script.js
+++ script.js
@@ -1,87 +1,126 @@
-let form = document.querySelector("form");
-let emailInp = document.querySelector("input[name='email']");
-let passInp = document.querySelector("input[type='password']");
-let phone = document.querySelector("input[type='number']");
-let btn = document.querySelector("button");
+// Access Inputs
 
+let emailInp = document.querySelector("input[type='email']");
+let passInp = document.querySelector("input[type='password']");
+let phoneInp = document.querySelector("input[type='number']");
+
+
+// Access input error p's :
+
+let allError = document.querySelectorAll(".error")
 let emailError = document.querySelector(".email-error");
-let passwordError = document.querySelector(".password-error");
+let passError = document.querySelector(".password-error");
 let phoneError = document.querySelector(".phone-error");
 
-//check input values are empty - till disable the button
+//access form and submit-btn
 
-emailInp.addEventListener("input", checkValidationForValues);
-passInp.addEventListener("input", checkValidationForValues);
-phone.addEventListener("input", checkValidationForValues);
+let form = document.querySelector("form");
+let btn = document.querySelector("button");
 
-let errorFlag = false;
+// Handling Input-time errors : 
 
-function checkValidationForValues() {
-    if (
-        emailInp.value.length < 3 ||
-        passInp.value.length < 3 ||
-        phone.value.length < 3
-    ) {
-        btn.disabled = true;
-    } else {
-        btn.disabled = false;
-    }
-}
 
-checkValidationForValues();
+emailInp.addEventListener("input", handleEmailInput);
 
-form.addEventListener("submit", formSubmit);
+passInp.addEventListener("input", handlePasswordInput);
 
-function formSubmit(e) {
+phoneInp.addEventListener("input", handlePhoneInput);
+
+form.addEventListener("submit", handleForm);
+
+function handleForm(e) {
+    handleEmailInput()
+    handlePasswordInput()
+    handlePhoneInput()
     e.preventDefault();
 
-    let formData = new FormData(form);
-    let email = formData.get("email");
-    let password = formData.get("password");
-    let phoneval = formData.get("phone");
 
-    checkError(email, password, phoneval);
-
-    if (errorFlag) {
-        alert("Error occured : please fix the errors");
+    if (![...allError].every(checkErrorExist)) {
+        console.log("Please check fields again !")
         return;
+    } else {
+        const formData = new FormData(form);
+        const email = formData.get("email");
+        const password = formData.get("password");
+        const phone = formData.get("phone");
+        console.log("FORM SUBMITTED")
+        console.log(`Values are : ${email}, ${password}, ${phone}`)
+
+        emailInp.value = "";
+        passInp.value = "";
+        phoneInp.value = "";
+
+        //reset border
+        emailInp.style.border = "";
+        passInp.style.border = "";
+        phoneInp.style.border = "";
     }
 
-    emailInp.value = "";
-    passInp.value = "";
-    phone.value = "";
-    console.log("login Success");
-    console.log(email);
-    console.log(password);
-    console.log(phoneval);
 }
 
-function checkError(email, password, phone) {
+function checkErrorExist(el) {
+    return el.classList.contains("hide");
+}
+
+function updateButtonState() {
+    const allGood = [...allError].every(checkErrorExist);
+    btn.disabled = !allGood;
+}
+
+
+
+function handleEmailInput() {
     const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
-
-    if (!emailRegex.test(email)) {
+    if ((emailInp.value).trim().length == 0) {
         emailError.classList.remove("hide");
-        emailError.textContent = "*Invalid email";
-        errorFlag = true;
-        return;
+        emailError.textContent = "Email can not empty";
+        emailInp.style.border = "2px solid red"
     }
-    if (password.length < 8) {
-        passwordError.classList.remove("hide");
+    else if (!emailRegex.test(emailInp.value)) {
+        emailError.classList.remove("hide");
+        emailError.textContent = "Please enter valid email";
+        emailInp.style.border = "2px solid red"
+    } else {
+        emailError.classList.add("hide");
+        emailError.textContent = "";
+        emailInp.style.border = "2px solid green"
+    }
 
-        passwordError.textContent = "Password must be 8 characters long";
-        errorFlag = true;
-        return;
+    updateButtonState()
+}
+
+function handlePasswordInput() {
+    if (passInp.value.length == 0) {
+        passError.classList.remove("hide");
+        passError.textContent = "Enter the password";
+        passInp.style.border = "2px solid red";
+    } else if (passInp.value.length < 8) {
+        passError.classList.remove("hide");
+        passError.textContent = "Password must be 8 chars long";
+        passInp.style.border = "2px solid red";
+    } else {
+        passError.classList.add("hide");
+        passError.textContent = "";
+        passInp.style.border = "2px solid green";
     }
-    if (phone.length < 10) {
+
+    updateButtonState()
+}
+
+function handlePhoneInput() {
+    if ((phoneInp.value).trim().length == 0) {
         phoneError.classList.remove("hide");
-
-        phoneError.textContent = "Phone number must be length of 10";
-        errorFlag = true;
-        return;
+        phoneError.textContent = "Phone cant empty";
+        phoneInp.style.border = "2px solid red";
+    } else if ((phoneInp.value).trim().length < 10) {
+        phoneError.classList.remove("hide");
+        phoneError.textContent = "Phone must be 10 chars long";
+        phoneInp.style.border = "2px solid red";
+    } else {
+        phoneError.classList.add("hide");
+        phoneError.textContent = "";
+        phoneInp.style.border = "2px solid green";
     }
 
-    errorFlag = false;
-    emailError.classList.add("hide");
-    passwordError.classList.add("hide");
-    phoneError.classList.add("hide");
+    updateButtonState()
 }
