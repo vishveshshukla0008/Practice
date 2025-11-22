45d42b135db955e5058fc0d995af183bb73d206f
Vishwesh Shukla <vishveshshukla82@gmail.com>
2025-11-22 17:47:35 +0530

    Piano

--- script.js
+++ script.js
@@ -1,7 +1,37 @@
-let likeIcon = document.querySelector("i");
+let allDiv = document.querySelectorAll("div");
+document.body.addEventListener("keydown", function (e) {
+    soundKey(e.key)
+});
 
-let img = document.querySelector("img");
+allDiv.forEach((el) => {
+    el.addEventListener("click", function (e) {
+        e.stopPropagation();
 
-img.addEventListener("dblclick", () => {
-    likeIcon.style.opacity = 1;
-})
+        const clickedElement = e.currentTarget;
+        const onlyOwnText = clickedElement.childNodes[0].nodeValue.trim();
+        soundKey(onlyOwnText);
+    });
+});
+
+const soundMap = {
+    "C": "./sounds/28.mp3",
+    "C#": "./sounds/29.mp3",
+    "D": "./sounds/30.mp3",
+    "D#": "./sounds/31.mp3",
+    "E": "./sounds/32.mp3",
+    "F": "./sounds/33.mp3",
+    "F#": "./sounds/34.mp3",
+    "G": "./sounds/39.mp3",
+    "G#": "./sounds/35.mp3",
+    "A": "./sounds/36.mp3",
+    "A#": "./sounds/37.mp3",
+    "B": "./sounds/38.mp3"
+};
+
+
+
+function soundKey(text) {
+    let audio = new Audio(soundMap[text]);
+    audio.play();
+}
+
