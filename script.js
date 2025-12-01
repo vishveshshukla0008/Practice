f09d3173842013b3284649bac424e62ec0854403
Vishwesh Shukla <vishveshshukla82@gmail.com>
2025-12-01 11:24:24 +0530

    MATRIX EFFECT COMPLETED

--- script.js
+++ script.js
@@ -1,5 +1,30 @@
-let box = document.querySelector(".box");
-document.addEventListener("mousemove", (e) => {
-    document.body.style.setProperty("--x", e.x+"px");
-    document.body.style.setProperty("--y", e.y+"px");
+let p = document.querySelector("p");
+
+let pText = p.innerText;
+
+let characters = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz'
+
+let iteration = 0;
+
+let interval = null;
+
+p.addEventListener("mouseenter", () => {
+    iteration = 0;
+    clearInterval(interval);
+    interval = setInterval(randomText, 24)
 })
+
+function randomText() {
+    let str = pText.split("")
+    let newArr = str.map((char, index) => {
+        if (iteration > index) {
+            return char;
+        }
+        return characters.split("")[Math.floor(Math.random() * 52)];
+    })
+    p.innerText = newArr.join("")
+    iteration += 0.25;
+    if (iteration > pText.length) {
+        clearInterval(interval);
+    }
+}
